f0be00323971979bfd62ee2291f2fff9217991e2
Lenz Gschwendtner <lenz@springtimesoft.com>
2015-02-02 13:07:35 +1300

    starting over

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-./rebar compile
-
-erl -pa ebin deps/*/ebin +A 4\
-    -eval "io:format(\"Point your browser at http://localhost:7654~n\")."\
-    -eval "couchbeam:start()."\
-    -eval "application:start(just_me)."
